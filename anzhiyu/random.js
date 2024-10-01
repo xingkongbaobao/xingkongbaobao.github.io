79ddf81e7cd21d10a1defea4f39673a2a2086292
xingkongbaobao <zhumengxingkong@outlook.com>
2024-10-02 02:08:30 +0800

    Site updated: 2024-10-02 02:08:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/20/hello-world/"];function toRandomPost(){
+var posts=["2024/09/20/hello-world/","2024/10/02/随笔/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
