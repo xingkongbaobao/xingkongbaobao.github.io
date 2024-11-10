ac9ba1e93a6d7baf7b2629282efb66c1e75d59af
xingkongbaobao <126758155+xingkongbaobao@users.noreply.github.com>
2024-11-10 02:26:14 +0000

    更改随笔 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/31/hello-world/","2024/10/02/随笔/"];function toRandomPost(){
+var posts=["2024/11/10/hello-world/","2024/10/02/随笔/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
